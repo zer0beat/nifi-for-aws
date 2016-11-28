072c99e87f6cc550ddd3939a66d675dcf077bbe3
Joel Llacer Martinez <z0beat@gmail.com>
2016-11-28 18:28:47 +0100

    Added interesting commands to nifi-configuration

--- nifi-configuration.sh
+++ nifi-configuration.sh
@@ -7,7 +7,12 @@ echo "NIFI_HOME=${NIFI_HOME}"
 echo "NiFi ${VERSION} configuration script"
 
 echo "Configure ${NIFI_HOME}/bin/nifi-env.sh"
-echo "export JAVA_HOME=${JAVA_HOME}" > ${NIFI_HOME}/bin/nifi-env.sh
+echo "export JAVA_HOME=${JAVA_HOME}" >> ${NIFI_HOME}/bin/nifi-env.sh
+
+#aws ec2 describe-instances --filters Name=tag:App,Values='Apache NiFi',Name=tag:aws:cloudformation:stack-name,Values=nifi --region eu-west-1
+#aws ec2 describe-instances --filters Name=tag:App,Values='Apache NiFi',Name=tag:aws:cloudformation:stack-name,Values=nifi --region eu-west-1 --query 'Reservations[*].Instances[*].[InstanceId]' --output text
+#aws ec2 describe-instances --filters Name=tag:App,Values='Apache NiFi',Name=tag:aws:cloudformation:stack-name,Values=nifi --region eu-west-1 --query 'Reservations[*].Instances[*].[PrivateDNSName]' --output text
+
 
 
 
